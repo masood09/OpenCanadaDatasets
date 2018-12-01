73874ec6557ab35103ae718b00e2a76259221a7c
Masood Ahmed <masood.ahmed@galepartners.com>
2018-12-01 10:42:29 -0500

    Adding test's to check the status code

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ResponseCodeTest(TestCase):
+    """The unit test cases for checking the correct response codes for various URL's"""
+
+    def test_refresh_api_valid_uuid(self):
+        """When the Refresh API is passed with valid UUID, the response code should be 201"""
+        response = self.client.get("/api/v1/refresh/fd3355a7-ae34-4df7-b477-07306182db69/")
+        self.assertEquals(response.status_code, 201)
+
+    def test_refresh_api_invalid_uuid(self):
+        """When the Refresh API is passed with invalid UUID, the response code should be 404"""
+        response = self.client.get("/api/v1/fd3355a7-ae34-4df7-b477-07306182db6/")
+        self.assertEquals(response.status_code, 404)
+
