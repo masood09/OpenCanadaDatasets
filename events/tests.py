28e43959dbc7c4fe06472459003ce7112110b702
Masood Ahmed <masood.ahmed@galepartners.com>
2018-12-01 11:06:24 -0500

    Adding model tests

--- events/tests.py
+++ events/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from .models import Event
+
 class ResponseCodeTest(TestCase):
     """The unit test cases for checking the correct response codes for various URL's"""
 
@@ -12,4 +14,18 @@ class ResponseCodeTest(TestCase):
         """When the Refresh API is passed with invalid UUID, the response code should be 404"""
         response = self.client.get("/api/v1/fd3355a7-ae34-4df7-b477-07306182db6/")
         self.assertEquals(response.status_code, 404)
+
+class EventModelTest(TestCase):
+    """The unit test cases for Event Model"""
+
+    fixtures = ["fixtures.json"]
+
+    def test_category_name_plural(self):
+        """Test whether the plural name is correct"""
+        self.assertEquals(str(Event._meta.verbose_name_plural), "events")
+
+    def test_string_representation(self):
+        """Test whether the string representation is equal to its name"""
+        event = Event.objects.get(pk="aeb4a545-a861-4785-9611-c2899902b2c3")
+        self.assertEquals(str(event), event.name)
 
